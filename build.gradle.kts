7eb25497e78a40eb859908befc4a15d58e9047af
RugbyGuy01 <goodmail@golfpvcc.com>
2023-12-18 09:37:32 -0500

    Project start fixed bugs

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,6 @@ buildscript {
     }
 }
 
-//plugins {
-//    id("com.android.application") version "8.1.1" apply false
-//    id("com.android.library") version "8.1.1" apply false
-//    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
-//    id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
-//}
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("com.android.library") version "8.2.0" apply false           //vpg 12/17/23
